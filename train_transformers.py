6fd2b94b0ed405f8c14edab882874b353c726a7f
CoGian <kgiantsios@atypon.com>
2021-05-13 19:06:38 +0300

    fix memory problem

--- train_transformers.py
+++ train_transformers.py
@@ -131,10 +131,26 @@ def get_dataset(PATH, mode=None, forTrain=False, forTest=False):
 			if mode == "under_sampling":
 				print("Under Sampling...")
 				X = np.dstack((input_ids, attention_mask))
-				del input_ids
-				del attention_mask
-				gc.collect()
-				X, labels = stratification_undersample(X, labels, per=0.66)
+				per = 0.66
+				n_samples = X.shape[0]
+				num_y1 = np.where(labels >= .5, 1, 0).sum()
+				num_y0 = n_samples - num_y1
+
+				filter_rand = np.random.rand(int(num_y1 + num_y0))
+
+				if num_y1 < num_y0:
+					num_y0_new = num_y1 * 1.0 / per - num_y1
+					num_y0_new_per = num_y0_new * 1.0 / num_y0
+					filter_0 = np.logical_and(labels < .5, filter_rand <= num_y0_new_per)
+					filter_ = np.nonzero(np.logical_or(labels >= .5, filter_0))[0]
+				else:
+					num_y1_new = num_y0 * 1.0 / per - num_y0
+					num_y1_new_per = num_y1_new * 1.0 / num_y1
+					filter_1 = np.logical_and(labels >= .5, filter_rand <= num_y1_new_per)
+					filter_ = np.nonzero(np.logical_or(labels < .5, filter_1))[0]
+
+				X = X[filter_, :]
+				labels = labels[filter_]
 				input_ids, attention_mask = X.T
 				del X
 				gc.collect()
